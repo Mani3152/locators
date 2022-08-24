124f8a7df85d4fbd7a40b509fc9c68f0ca9ef50a
Mani3152 <111729068+Mani3152@users.noreply.github.com>
2022-08-24 18:53:09 +0530

    Add files via upload

---
+++ src/main/java/com/locator/test1.java
@@ -0,0 +1,28 @@
+package com.locator;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://demo.automationtesting.in/Register.html");
+        driver.findElement(B)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
